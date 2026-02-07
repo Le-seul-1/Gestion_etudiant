3e37f88c20ac1758b5b4dbe63cbd219f81b7bd01
Le-seul-01 <ishimwebonheurchris1@gmail.com>
2026-02-07 18:58:50 +0100

    creation des modeles et de gitignore

--- Gestion_etudiant/models.py
+++ Gestion_etudiant/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Etudiant(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    nom = models.CharField(max_length=100)
+    prenom = models.CharField(max_length=100)
+    num_matricule = models.CharField(max_length=20, unique=True)
+
+class Enseignement(models.Model):
+    id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=100)
+    code = models.CharField(max_length=20, unique=True)
+    classe = models.ForeignKey('Annee', on_delete=models.CASCADE)
+
+class Annee(models.Model):
+    id = models.AutoField(primary_key=True)
+    annee_scolaire = models.CharField(max_length=20, unique=True)
+    filieres = models.CharField(max_length=150)
+
+class Semestre(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    nom = models.CharField(max_length=100)
+    annee = models.ForeignKey(Annee, on_delete=models.CASCADE)
+
+class Evaluation(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    etudiant = models.ForeignKey(Etudiant, on_delete=models.CASCADE)
+    enseignement = models.ForeignKey(Enseignement, on_delete=models.CASCADE)
+    semestre = models.ForeignKey(Semestre, on_delete=models.CASCADE)
+    note = models.FloatField()
+
+    class Meta:
+        unique_together = ('etudiant', 'enseignement', 'semestre')
