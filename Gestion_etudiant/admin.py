6552092825b99dd7b4b59bc4d294e3b40f9acf33
Le-seul-01 <ishimwebonheurchris1@gmail.com>
2026-02-22 20:01:35 +0100

    Clean project with good gitignore and well recovered

--- Gestion_etudiant/admin.py
+++ Gestion_etudiant/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from .models import Etudiant, Enseignement, Annee, Semestre, Evaluation
 
 # Register your models here.
+@admin.register(Etudiant)
+
+class EtudiantAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'prenom', 'num_matricule')
+    search_fields = ('nom', 'prenom', 'num_matricule')
+
+
+@admin.register(Enseignement)
+
+class EnseignementAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'code', 'classe')
+    search_fields = ('nom', 'code')
+    list_filter = ('classe',)
+
+
+@admin.register(Annee)
+
+class AnneeAdmin(admin.ModelAdmin):
+    list_display = ('annee_scolaire', 'filieres')
+    search_fields = ('annee_scolaire',)
+
+
+@admin.register(Semestre)
+
+class SemestreAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'annee')
+    list_filter = ('annee',)
+
+
+@admin.register(Evaluation)
+
+class EvaluationAdmin(admin.ModelAdmin):
+    list_display = ('etudiant', 'enseignement', 'semestre', 'note')
+    list_filter = ('semestre', 'enseignement')
+    search_fields = ('etudiant__nom', 'enseignement__nom')
+    list_select_related = ('etudiant', 'enseignement', 'semestre')
+    ordering = ('-note',)
+
+
+
--- Gestion_etudiant/models.py
+++ Gestion_etudiant/models.py
@@ -7,21 +7,37 @@ class Etudiant(models.Model):
     prenom = models.CharField(max_length=100)
     num_matricule = models.CharField(max_length=20, unique=True)
 
+    def __str__(self):
+        return f"{self.nom} {self.prenom}"
+
+
 class Enseignement(models.Model):
     id = models.AutoField(primary_key=True)
     nom = models.CharField(max_length=100)
     code = models.CharField(max_length=20, unique=True)
     classe = models.ForeignKey('Annee', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.nom} ({self.code})"
+
+
 class Annee(models.Model):
     id = models.AutoField(primary_key=True)
     annee_scolaire = models.CharField(max_length=20, unique=True)
     filieres = models.CharField(max_length=150)
 
+    def __str__(self):
+        return self.annee_scolaire
+
+
 class Semestre(models.Model):
     id = models.BigAutoField(primary_key=True)
     nom = models.CharField(max_length=100)
     annee = models.ForeignKey(Annee, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.nom} - {self.annee}"
+
 
 class Evaluation(models.Model):
     id = models.BigAutoField(primary_key=True)
@@ -31,4 +47,13 @@ class Evaluation(models.Model):
     note = models.FloatField()
 
     class Meta:
-        unique_together = ('etudiant', 'enseignement', 'semestre')
+            constraints = [
+                models.UniqueConstraint(
+                    fields=['etudiant', 'enseignement', 'semestre'],
+                    name='unique_evaluation'
+                )
+            ]
+
+
+    def __str__(self):
+        return f"{self.etudiant} - {self.enseignement} - {self.note}"
